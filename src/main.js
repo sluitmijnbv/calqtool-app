0f85a787f5c3333bef0a045963a60f8c8f2250d2
Ecomsoftline <ecomsoftline@gmail.com>
2026-02-20 02:58:03 +0100

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -1,17 +1,19 @@
 import './styles.css'
+
+import { renderLayout } from './ui/layout.js'
 import { initState } from './core/state.js'
 import { restore } from './modules/performance.js'
 
 import './modules/plan.js'
 import './modules/project.js'
-import './modules/builder.js'
 import './modules/report.js'
 import './modules/assistant.js'
 import './modules/drawing.js'
-import './modules/policy.js'
 import './modules/performance.js'
 
-import './ui/layout.js'
+import './modules/builder.js'
+import './ui/builderView.js'
 
+renderLayout()
 initState()
 restore()
